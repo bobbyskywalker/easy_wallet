88098805780e13462a7fb75eb10eabbe03da99c5
Maciej Wiącek <maciejwiacek@Maciejs-MacBook-Air.local>
2025-05-31 12:33:16 +0200

    SwiftUI init

---
+++ EasyWallet/EasyWallet/EasyWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyWalletApp.swift
+//  EasyWallet
+//
+//  Created by Maciej Wiącek on 31/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct EasyWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
