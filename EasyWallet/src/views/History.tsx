bdac5f6c80c515e45c72d536886a60066de866d2
dszafran <dszafran@st.swps.edu.pl>
2025-05-31 22:34:32 +0200

    history enhanced

--- EasyWallet/src/views/History.tsx
+++ EasyWallet/src/views/History.tsx
@@ -1,27 +1,34 @@
-import BottomNavBar from '../components/BottomNavBar'
-import { TokenBar } from '../components/TokenBar'
-import { TransactionCard } from '../components/TransactionCard'
-import { ReactComponent as Succes } from '../assets/success.svg'
-
+import React from 'react';
+import BottomNavBar from '../components/BottomNavBar';
+import TransactionItem from '../components/TransactionItem';
 
 const History = () => {
-	return (
-		<div className='min-h-screen bg-black text-white px-4 py-6 flex flex-col items-center'>
-			<h1 className='text-xl font-semibold mb-6'>History</h1>
-			<p className='text-gray-500'>This is the history page.</p>
-			<TokenBar
-			imageUrl="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"
-			symbol="BTC"
-			name="Bitcoin"
-			value="3.0"
-			usdValue="3000"
-			change="+2.5%"
-			/>
-			<div className='w-full bottom-0 fixed'>
-				<BottomNavBar active='history' />
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className='min-h-screen bg-black text-white px-4 py-6 flex flex-col items-center'>
+      <h1 className='text-xl font-semibold mb-6'>History</h1>
 
-export default History
+      <div className='w-full max-w-md flex flex-col gap-4'>
+        <TransactionItem
+          imageUrl="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"
+          label="Sent BTC"
+          status="Confirmed"
+          usdAmount="$52.96"
+          tokenAmount="0.02 BTC"
+        />
+        <TransactionItem
+          imageUrl="https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png"
+          label="Received ETH"
+          status="Pending"
+          usdAmount="$140.20"
+          tokenAmount="0.07 ETH"
+        />
+      </div>
+
+      <div className='w-full bottom-0 fixed'>
+        <BottomNavBar active='history' />
+      </div>
+    </div>
+  );
+};
+
+export default History;
