68e1ac2e7646d177c1abc1deb76745f1fb2efdac
dszafran <dszafran@st.swps.edu.pl>
2025-05-31 22:28:08 +0200

    updated Market and added searchbar component

--- EasyWallet/src/views/Market.tsx
+++ EasyWallet/src/views/Market.tsx
@@ -1,15 +1,40 @@
-import BottomNavBar from '../components/BottomNavBar'
+import React from 'react';
+import BottomNavBar from '../components/BottomNavBar';
+import SearchBar from '../components/SearchBar.tsx';
+import { TokenBar } from '../components/TokenBar';
+
+const mockTokens = Array(6).fill({
+  imageUrl: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png',
+  symbol: 'BTC',
+  name: 'Bitcoin',
+  value: '3.00912',
+  usdValue: '3000',
+  change: '(+0.68%)',
+  changePositive: true,
+  chart: <img src="/chart-placeholder.png" alt="chart" className="h-8" />,
+});
 
 const Market = () => {
-	return (
-		<div className='flex flex-col items-center justify-center h-screen'>
-			<h1 className='text-2xl font-bold mb-4'>Market</h1>
-			<p className='text-gray-500'>This is the Market view.</p>
-			<div className='w-full bottom-0 fixed'>
-				<BottomNavBar active='market' />
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="min-h-screen bg-black text-white px-4 pt-6 pb-20 flex flex-col items-center">
+      <h1 className="text-2xl font-semibold mb-4 w-full text-left">Market</h1>
 
-export default Market
+      <div className="w-full max-w-md mb-6">
+        <SearchBar />
+      </div>
+
+      <div className="w-full max-w-md flex flex-col gap-4">
+        {mockTokens.map((token, index) => (
+          <TokenBar key={index} {...token} />
+        ))}
+      </div>
+
+      {/* Bottom Navigation */}
+      <div className='w-full bottom-0 fixed'>
+        <BottomNavBar active='market' />
+      </div>
+    </div>
+  );
+};
+
+export default Market;
